0b4bb5899a8476e7a3faf018873a0aad74db443a
John Snyder <vakama8@yahoo.com>
2019-09-30 13:01:41 -0500

    Lab Assignments

---
+++ src/ActivityOne.py
@@ -0,0 +1,26 @@
+def countup(n):
+    if n >= 0:
+        countup(n - 1)
+        print(n)
+
+#countup(5)
+
+
+# Define the recursive function
+def indexOf(text,string):
+    L = len(text)
+    if string:
+        print('elif ' + text + ' == ' + string[0])
+    if len(string) == 0:
+        return -1
+    elif text == string[:L]:
+        return 0
+    else:
+        return 1 + indexOf(text, string[1:])
+
+# Write main function to verify
+text1 = 'no'
+string1 = 'noodle'
+
+print(indexOf(text1,string1))
+#SHOULD return 6 but returns 10
