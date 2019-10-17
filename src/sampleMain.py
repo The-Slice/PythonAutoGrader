1f8c067cc9eb03ba28cc0479ee73ad2b8efa1ccb
Bryce Bjorkman <bjorkmba7940@uwec.edu>
2019-10-17 07:49:29 -0500

    Delete sampleMain.py

--- src/sampleMain.py
+++
@@ -1,8 +0,0 @@
-""" sampleMain.py
-just the dumbest script for internal testing
-@author Bryce Bjorkman
-"""
-if __name__=="__main__":
-    print("made it into sampleMain")
-    print("MADE IT")
-    print("MAAAAAAAADE it")
