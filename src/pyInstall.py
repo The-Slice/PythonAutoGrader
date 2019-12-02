ebd2f4fd664f03c24a6ffd1cb354957e917bea86
shane brandl <brandlsr6535@uwec.edu>
2019-12-01 21:45:23 -0600

    Revert back to working state - Shane 12/1/19 - 9:44pm

--- src/pyInstall.py
+++ src/pyInstall.py
@@ -8,49 +8,35 @@ from zipfile import ZipFile
 from shutil import copy2
 from shutil import copy
 import subprocess
-
-SRCROOT = os.path.dirname(os.path.realpath(sys.argv[0]))
-REPOROOT = os.path.dirname(SRCROOT)
-subprocess.run(["python", "-m", "pip", "install", "-r", os.path.join(REPOROOT, "config\\pyreqs.txt")]);
-
-
-
 import pip
 import winshell
 import pyshortcuts
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-
 import PyInstaller.__main__
 import PyInstaller.config
 
+SRCROOT = os.path.dirname(os.path.realpath(sys.argv[0]))
+REPOROOT = os.path.dirname(SRCROOT)
+subprocess.run(["python", "-m", "pip", "install", "-r", os.path.join(REPOROOT, "config\\pyreqs.txt")]);
 
 
-
- 
 class MyWindow(QMainWindow):
- 
     path = "path"
- 
+
     def __init__(self):
-	
         super().__init__()
-        self.setWindowTitle('Autograder Install Wizard') 
-	
-        
-		       
+        self.setWindowTitle('Autograder Install Wizard')
         self.setWindowIcon(QIcon('../img/pythonBlugold.ico'))
-		
         self.buildButton = QPushButton("Build", self)
         self.buildButton.setEnabled(False)
-        self.buildButton.resize(50,32)	
-        
+        self.buildButton.resize(50, 32)
         self.buildButton.clicked.connect(self.buildExecutable)
         self.browseButton = QPushButton("Browse", self)
-        self.buildButton.resize(50,32)
+        self.buildButton.resize(50, 32)
         self.browseButton.clicked.connect(self.openDirectory)
-		
+
         self.resultArea = QPlainTextEdit(self)
         self.resultArea.setReadOnly(True)
         self.resultArea.setPlainText("Add a path with browse. . .")
@@ -59,29 +45,27 @@ class MyWindow(QMainWindow):
         self.desktopShortcut.setChecked(True)
         self.startbar = QCheckBox("Create start menu shortcut", self)
         self.startbar.setChecked(True)
-					
+
         layout = QVBoxLayout()
+        layout.addWidget(self.buildButton)
+        layout.addWidget(self.resultArea)
         layout.addWidget(self.browseButton)
-        layout.addWidget(self.resultArea)	
-        layout.addWidget(self.buildButton)	
         layout.addWidget(self.desktopShortcut)
         layout.addWidget(self.startbar)
-		
-        		
-	    
+
         widget = QWidget()
         # widget.setLayout(horizontalLayout)
-        self.setCentralWidget(widget)    
-        
+        self.setCentralWidget(widget)
+
         # dock = QDockWidget()
         # dock.setWidget(self.resultArea)
-		
+
         # self.addDockWidget(Qt.TopDockWidgetArea, dock)
-        
+
         widget.setLayout(layout)
-		
+
         self.show()
-	
+
     def appendPlainText(self, text2Append, end='\n'):
         current = self.resultArea.toPlainText()
         new = current + end + text2Append
@@ -95,16 +79,15 @@ class MyWindow(QMainWindow):
     def buildExecutable(self):
         self.buildButton.setEnabled(False)
         self.browseButton.setEnabled(False)
-        
+
         self.appendPlainText('Installing prerequisite modules ... ')
         self.appendPlainText("done", end='')
         self.appendPlainText("Compiling script ... ")
-        if(self.path != 'path'):
+        if (self.path != 'path'):
             args = [
                 '-F',
                 'autograder.py',
                 'commentSummary.py',
-                'guiutil.py',
                 'tester.py',
                 '--onefile',
                 '--distpath', '%s/bin' % self.path,
@@ -149,29 +132,29 @@ class MyWindow(QMainWindow):
         )
         resp = QMessageBox.question(self, 'Install successful', "You may new exit the program", QMessageBox.Ok)
         app.quit()
-		
-		
-	
+
     def openDirectory(self):
-	
+
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName= QFileDialog.getExistingDirectory(self,"Please Select a Directory", options=options)
-        self.resultArea.setPlainText(fileName)		
+        fileName = QFileDialog.getExistingDirectory(self, "Please Select a Directory", options=options)
+        self.resultArea.setPlainText(fileName)
         self.path = fileName
         self.buildButton.setEnabled(True)
+
 
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
-        return False	
-		
+        return False
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     app_icon = QIcon('.img/pythonBlugold.ico')
     app.setWindowIcon(app_icon)
     ex = MyWindow()
     sys.exit(app.exec_())
-    #TODO: DIRECTORY PERMISSION CHECKING
-		
+    # TODO: DIRECTORY PERMISSION CHECKING
+
